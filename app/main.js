428561b2eb3289a0983950ca1f241c9dd27db09f
Cole Krumbholz <cole.krumbholz@gmail.com>
2013-02-20 16:06:34 -0800

    new starting point for tut2

--- app/main.js
+++ app/main.js
@@ -1,11 +1,1 @@
-var PoemView = Backbone.View.extend({
-  render: function() {
-    this.$el.html(Handlebars.templates.poem());
-    return this;
-  }
-});
-
-$(function() {
-  var poemView = new PoemView();
-  $("body").append(poemView.render().el);  
-});
+// TODO: Make the Javascripts!!
